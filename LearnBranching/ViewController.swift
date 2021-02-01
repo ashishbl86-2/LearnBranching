0b9e2a8ed06b97375d54e758d078b90163a5b3a5
Ashish Bansal <ashishbl86@gmail.com>
2021-02-01 09:23:22 +0530

    Prepared base

--- LearnBranching/ViewController.swift
+++ LearnBranching/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        // TODO: Need to add some code here
     }
 
-
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
 }
 
